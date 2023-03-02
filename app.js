4c5890fd3058140f8e8afdcbaca0bdf0b02cee2a
Korab <korab.morina444@gmail.com>
2023-03-02 21:25:05 +0100

    Get data from external source, tourn them to JSON.parse and access specific data inside object

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ app.get('/', function(req, res) {
 
     var htttpWeather = 'https://api.openweathermap.org/data/2.5/weather?q=Prishtina&appid=c6f41b6b88d80af7ceda9140000b0c74&units=metric';
     https.get(htttpWeather, function(response) {
-        console.log(response)
+        console.log(response.statusCode)
+        response.on('data', function(data) {
+            const weatherData = JSON.parse(data);
+            var temp = weatherData.main.temp;
+            var tempMin = weatherData.main.temp_min;
+            var tempMax = weatherData.main.temp_max;
+            var feelsLike = weatherData.main.feels_like
+            console.log(temp, tempMin, tempMax, feelsLike)
+        })
     })
     res.send('Server is up and running')
 })
